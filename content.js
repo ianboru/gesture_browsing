7488a70b97902265e344c47ca4a2d9ca9c3b4254
ian b <ianboru@gmail.com>
2018-12-03 10:16:17 -0500

    gesture on off in top right

--- content.js
+++ content.js
@@ -1,17 +1,46 @@
+console.log("running content")
 var bubbleDOM = document.createElement('div');
 document.body.style.position = 'relative'
 document.body.appendChild(bubbleDOM);
 bubbleDOM.style.visibility = 'hidden';
 bubbleDOM.id = "selectBubble"
 bubbleDOM.height = "200px";
-bubbleDOM.style.top = '50%';
-bubbleDOM.style.left = '46%';
+bubbleDOM.style.right = '10px';
+bubbleDOM.style.top = '10px';
+bubbleDOM.style.padding = '4px 8px';
+
 bubbleDOM.style.position = 'fixed';
-bubbleDOM.style.zIndex = '1000'
+bubbleDOM.style.zIndex = '2147483647'
 bubbleDOM.style.background = 'blue'
 bubbleDOM.style.fontSize = '20pt'
 bubbleDOM.style.color = 'white'
 bubbleDOM.style.borderRadius = '3px'
+
+
+  /*
+  background: rgba(0, 0, 0, 0.9);
+  background: -moz-linear-gradient(top, rgba(30, 30, 30, 0.95), rgba(0, 0, 0, 0.9));
+  background: -webkit-gradient(linear, 0 0, 0 100%, from(rgba(30, 30, 30, 0.95)), to(rgba(0, 0, 0, 0.9)));
+  border: 1px solid black;
+  border-radius: 5px;
+  -moz-border-radius: 5px;
+  -webkit-border-radius: 5px;
+  box-shadow: inset 0 0 1px #555, 0 0 5px #000;
+  -moz-box-shadow: inset 0 1px 0 #555, 0 0 5px #000;
+  -webkit-box-shadow: inset 0 1px 0 #555, 0 0 5px #000;
+  color: #fff;
+  font-family: "Helvetica Neue", sans-serif;  
+  font-size: 14px;
+  line-height: 1.286;  
+  padding: 0;
+  text-shadow: 0 -1px 0 #111;
+  box-shadow: inset 0 0 1px #555, 0 0 2px #000;
+  -moz-box-shadow: inset 0 1px 0 #555, 0 0 2px #000;
+  -webkit-box-shadow: inset 0 1px 0 #555, 0 0 2px #000;  
+
+  */
+  
+  
 // Move that bubble to the appropriate location.
 function renderBubble() {
   bubbleDOM.style.visibility = 'visible';
